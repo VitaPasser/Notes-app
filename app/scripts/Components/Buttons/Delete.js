7a291e6123d9fdeee2a04a23d169a0d14aaa0c5e
VitaPasser <vitapasser@gmail.com>
2023-07-27 11:00:20 +0300

    Fixed bug with not true icons place

--- app/scripts/Components/Buttons/Delete.js
+++ app/scripts/Components/Buttons/Delete.js
@@ -5,6 +5,9 @@ const addDeleteButtons = (app, data, config) =>
     addButtons(app, data, config, deleteButton)
 
 const deleteButton = function (app, data, id, config) {
+    const noteApp = app.querySelector(`tr[id$='${id}']`)
+    if (noteApp == null) return;
+    
     const nodeTd = document.createElement('td')
     const noteDeleteButton = document.createElement('i')
 
@@ -12,8 +15,6 @@ const deleteButton = function (app, data, id, config) {
     noteDeleteButton.id = `delete-${id}`
     noteDeleteButton.addEventListener('click', deleteNote.bind(null, data, id, config))
 
-    const noteApp = app.querySelector(`tr[id$='${id}']`)
-    if (noteApp == null) return;
     nodeTd.append(noteDeleteButton)
     noteApp.append(nodeTd)
 }
